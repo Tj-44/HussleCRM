01a7a1543449281d641da08eac2344de74b83d2a
Tj-44 <taranjit.a@icloud.com>
2025-07-11 01:59:36 -0700

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+
+export const clinicSettings = {
+  doctors: ["Dr. Gill", "Dr. Smith"],
+  petTypes: ["Dog", "Cat", "Rabbit"],
+  appointmentReasons: ["Checkup", "Surgery", "Vaccines"],
+  statuses: ["On Time", "+15 Min Late", "Seen", "No Show"]
+};
